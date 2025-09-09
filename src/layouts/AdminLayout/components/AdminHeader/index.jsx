6c550f1aaf4222fb9a86128b11180235665f01ad
Nguyen Khanh Duy <21522003@gm.uit.edu.vn>
2025-09-09 12:43:03 +0700

    fix link to img public

--- src/layouts/AdminLayout/components/AdminHeader/index.jsx
+++ src/layouts/AdminLayout/components/AdminHeader/index.jsx
@@ -3,9 +3,9 @@ import styles from "./AdminHeader.module.scss"
 function Header() {
     return (
         <div className={styles.container}>
-            <img src="/logo.png" className={styles.logo} />
+            <img src="logo.png" className={styles.logo} />
             <h2 className={styles.heading}>Header của Admin</h2>
-            <img src="/user-placeholder.png" className={styles.user} />
+            <img src="user-placeholder.png" className={styles.user} />
         </div>
     )
 }
